bdcfe6b278a63dd0d700329189f5a57dd5a56497
SyKoHPaTh <sykohpath@gmail.com>
2012-07-24 08:11:25 -0400

    controller database test

---
+++ controller/users.php
@@ -0,0 +1,13 @@
+<?php
+	$sql = "SELECT * FROM users";
+	$result = mysqli_query($websitetest_db, $sql);
+	if(!$result){
+		die('SQL Error: ' . mysqli_error());
+	}
+
+	while($row = mysqli_fetch_assoc($result)){ 
+		echo $row['name']; //list all names
+	}
+
+	mysqli_free_result($result);
+?>
