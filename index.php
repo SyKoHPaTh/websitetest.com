11ff91f1a69ddc1b01913c25411e6188cc138d8e
SyKoHPaTh <sykohpath@gmail.com>
2012-07-24 07:53:05 -0400

    test including db settings out of webroot

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
-I'm awesome!
+<?php
+	//require "db_connect.php"; //load config from /includes if pathed in php.ini
+		//OR
+	require "../../includes/db_connect.php"; //<yoursite.com>/websitetest.com/index.php
+		//OR
+	//require __DIR__ . "/includes/db_connect.php";
 
-Super Awesome!
+	require "controller/users.php"; //pass to controller
+?>
