ca3ce5a939c59d669ff591244fbe9f7e261d897d
SyKoHPaTh <sykohpath@gmail.com>
2012-07-30 08:33:10 -0400

    read the third part of the url, check if that exists, if not, error, else say your'e passing control

--- index.php
+++ index.php
@@ -1,19 +1,23 @@
 <?php
-	//require "db_connect.php"; //load config from /includes if pathed in php.ini
-		//OR
-	require "../../includes/db_connect.php"; //<yoursite.com>/websitetest.com/index.php
-		//OR
-	//require __DIR__ . "/includes/db_connect.php";
+	require "../../includes/db_connect.php"; //modify to your needs
 
 	$url = "http://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
 
 	$urlParse = parse_url($url);
 
-	print_r($urlParse);
-
 	$urlParts = explode('/', $urlParse['path']);
 
-	print_r($urlParts);
+	if(isset($urlParts[3])){
+		//check if file exists
+		$controller = "controller/" . $urlParts[3] . ".php";
+		if(!file_exists($controller)){
+			die("Controller not found: " . $controller);
+		}
+	} else {
+		$controller = "controller/index.php";
+	}
+
+	echo "I'm gonna pass control to: ", $controller;
 
 	//require "controller/index.php"; //pass to controller
 ?>
