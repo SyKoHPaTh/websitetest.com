bc12a1b30b7da7dbe727f717449ee78ef7f84d91
SyKoHPaTh <sykohpath@gmail.com>
2012-07-30 09:55:46 -0400

    redo links

--- view/index.php
+++ view/index.php
@@ -5,11 +5,11 @@
 <body>
 	<?php if($_SESSION["logged_in"]=="YES"): ?>
 		Welcome Back, <?php echo $_SESSION['name']; ?>!<br>
-		<a href="index.php/logout">Logout</a><br>
+		<a href="logout">Logout</a><br>
 	<?php else: ?>
-		<a href="index.php/login">Login</a><br>
+		<a href="login">Login</a><br>
 	<?php endif; ?>
-	<a href="index.php/register">Register</a><br>
+	<a href="register">Register</a><br>
 	<?php print_r($_SESSION); ?>
 </body>
 </html>
