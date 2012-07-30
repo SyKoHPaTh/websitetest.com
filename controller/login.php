54f99e14b8d21632a90d1284dba9ad5c7faea8ec
SyKoHPaTh <sykohpath@gmail.com>
2012-07-30 09:06:56 -0400

    had view call in wrong spot

--- controller/login.php
+++ controller/login.php
@@ -10,32 +10,34 @@
 
 
 	//login user
-		if(isset($_POST['submit'])){
-			//setup array to match table fields=>form values
-			$form = array(
-				"username" => filter_var($_POST['username'], FILTER_SANITIZE_STRING),
-				"password" => filter_var($_POST['password'], FILTER_SANITIZE_STRING)
-				);
+	if(isset($_POST['submit'])){
+		//setup array to match table fields=>form values
+		$form = array(
+			"username" => filter_var($_POST['username'], FILTER_SANITIZE_STRING),
+			"password" => filter_var($_POST['password'], FILTER_SANITIZE_STRING)
+			);
 
-			//secure/encrypt the password
-			$salt = "pepper";
-			$form['password'] = md5($salt + md5($form['password']));
+		//secure/encrypt the password
+		$salt = "pepper";
+		$form['password'] = md5($salt + md5($form['password']));
 
 
-			//check if user and password matches
-			$errors = validate_user($websitetest_db, $form);
+		//check if user and password matches
+		$errors = validate_user($websitetest_db, $form);
 
-			if(isset($errors['id'])){
-				//if so, cram that user into session
-				$_SESSION['logged_in'] = "YES";
-				$_SESSION['username'] = $errors['username'];
-				$_SESSION['name'] = $errors['name'];
+		if(isset($errors['id'])){
+			//if so, cram that user into session
+			$_SESSION['logged_in'] = "YES";
+			$_SESSION['username'] = $errors['username'];
+			$_SESSION['name'] = $errors['name'];
 
-			} else {
-				//user not found, or incorrect password!
-			}
+			//redirect to index page
+			redirect("index.php");
 
-			require "view/user_login.php";
+		} else {
+			//user not found, or incorrect password!
 		}
+	}
+	require "view/user_login.php";
 
 ?>
