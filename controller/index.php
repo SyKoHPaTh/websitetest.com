ba7b4006ded6d40bcf1e8fd387a3c4ebc24e24ec
SyKoHPaTh <sykohpath@gmail.com>
2012-07-30 11:16:05 -0400

    check for completed url for links

--- controller/index.php
+++ controller/index.php
@@ -1,5 +1,10 @@
 <?php
 
+	if($urlParts[2] == ""){
+		//index.php was not supplied, so modify links to reflect that
+		$prefix = "index.php/";
+	}
+
 	require "view/index.php";
 
 ?>
--- view/index.php
+++ view/index.php
@@ -1,13 +1,15 @@
-<html>
+<?php
+	if(!isset($prefix)){ $prefix = ""; } //init
+?><html>
 <head>
 	<title>User Management System</title>
 </head>
 <body>
 	<?php if($_SESSION["logged_in"]=="YES"): ?>
 		Welcome Back, <?php echo $_SESSION['name']; ?>!<br>
-		<a href="logout">Logout</a><br>
+		<a href="<?php echo $prefix; ?>logout">Logout</a><br>
 	<?php else: ?>
-		<a href="login">Login</a><br>
+		<a href="<?php echo $prefix; ?>login">Login</a><br>
 	<?php endif; ?>
 	<a href="register">Register</a><br>
 	<?php print_r($_SESSION); ?>
